f7042a5cd121e62ee45f83569a96a9a7c6b3f72f
Nuno Coelho Santos <nuno@nunocoelhosantos.com>
2013-07-29 22:54:50 +0100

    Added responsive images function and updated cover images size

---
+++ source/javascripts/_fetch-high-resolution-images.js
@@ -0,0 +1,33 @@
+function fetchHighResolutionImages() {
+
+  /**
+   * After page is loaded, check the viewport size and display density.
+   * If the window size is bigger than 750px, load the medium size assets.
+   * If window size is bigger than 750px and retina, or bigger than 1500px,
+   * load the higher resolution images.
+   */
+
+  var viewpirtWidth = $(window).width();
+  var mediumImageSourceAttribute = 'data-image-medium-src';
+  var largeImageSourceAttribue = 'data-image-large-src';
+  var isHighDensity = (window.devicePixelRatio >= 2) ? true : false;
+
+  $(window).load(function() {
+
+    if (viewpirtWidth > 750) {
+      if (isHighDensity || viewpirtWidth > 1500) {
+        $('img').each(function() {
+          var img = $(this);
+          img.attr('src', img.attr(largeImageSourceAttribue));
+        });
+      } else {
+        $('img').each(function() {
+          var img = $(this);
+          img.attr('src', img.attr(mediumImageSourceAttribute));
+        });
+      }
+    };
+
+  });
+
+}
