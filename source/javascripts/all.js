bd480db49c6fbaaf67e97f86340a6c070e1cd235
Nuno Coelho Santos <nuno@ustwo.co.uk>
2013-08-17 20:21:14 +0100

    New logo, logo fading out with Skrollr, removed jQuery.

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,28 +1,48 @@
-//= require_tree .
-
 /**
- * Calling methods.
+ * Functions.
  */
 
-$(document).ready(function() {
 
-  // Initialize Skrollr.
-  skrollr.init();
+function fetchHighResolutionImages() {
 
+  /**
+   * After page is loaded, check the viewport size and display density.
+   * If the window size is bigger than 750px, load the medium size assets.
+   * If window size is bigger than 750px and retina, or bigger than 1500px,
+   * load the higher resolution images.
+   */
+
+  var viewpirtWidth = $(window).width();
+  var mediumImageSourceAttribute = 'data-image-medium-src';
+  var largeImageSourceAttribue = 'data-image-large-src';
+  var isHighDensity = (window.devicePixelRatio >= 2) ? true : false;
+
+  if (viewpirtWidth > 750) {
+    if (isHighDensity && viewpirtWidth > 1500) {
+      $('img').each(function() {
+        var img = $(this);
+        img.attr('src', img.attr(largeImageSourceAttribue));
+      });
+    } else {
+      $('img').each(function() {
+        var img = $(this);
+        img.attr('src', img.attr(mediumImageSourceAttribute));
+      });
+    }
+  };
+
+}
+
+/**
+ * Initializations.
+ */
+
+// Initialize Skrollr.
+skrollr.init({
+  smoothScrollingDuration: 600,
+  forceHeight: false
 });
 
 $(window).load(function() {
-
   fetchHighResolutionImages();
-
-});
-
-$(window).scroll(function(){
-
-  if ($(window).scrollTop() <= 150) {
-    $('h1').fadeIn(200);
-  } else {
-    $('h1').fadeOut(200);
-  }
-
 });
